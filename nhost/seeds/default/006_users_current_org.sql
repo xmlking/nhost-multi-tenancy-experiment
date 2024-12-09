d62860dafe4ea43590e890e6cd9f932567790620
Sumanth Chinthagunta <xmlking@gmail.com>
2024-12-08 19:32:23 -0800

    fix: aito enroll org_owner when new org is created

--- nhost/seeds/default/006_users_current_org.sql
+++
@@ -1,3 +0,0 @@
-SET check_function_bodies = false;
-UPDATE public.user_org_roles SET is_current_org = true WHERE user_id = '572ad1c0-f97b-4e16-b1f6-8b5ca90f931f' and org_id = '8dfd9a31-de01-47be-92a7-ba1c720c6270';
-UPDATE public.user_org_roles SET is_current_org = true WHERE user_id = 'bacd19f4-0cc4-43d1-9e7a-4e5098ed8d83' and org_id = 'd5dbb6b6-5e43-4dca-b855-be9b65b6695b';
